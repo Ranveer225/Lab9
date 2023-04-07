ac633f6391592916187484f3fa0140dbafafb467
Ranveer Singh PC <Ranveer Singh PC@RanveerSingh-PC.lan>
2023-04-07 11:27:22 -0400

    Identify triangle number that has more than one hundred divisors

--- src/main/java/Homework09.java
+++ src/main/java/Homework09.java
@@ -28,7 +28,13 @@ We can see that 28 is the first triangle number to have over five divisors.
 
 What is the value of the first triangle number to have over one hundred divisors?
     
+  
     */
     
-    
+    public static long
+    findTriangleNumberWithOverNDivisors(int n){
+    int count = 0;        
+        return 0;
 }
+}
+
